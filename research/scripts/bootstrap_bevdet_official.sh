879d7ba38b6ed516c540a5c32fe83b036970a05d
Achyut Boggaram <achbogga@gmail.com>
2026-04-09 23:08:15 -0500

    Drop obsolete conda TOS injection for py38 bootstrap

--- research/scripts/bootstrap_bevdet_official.sh
+++ research/scripts/bootstrap_bevdet_official.sh
@@ -44,15 +44,6 @@ for line in source:
             "/opt/conda/bin/conda install -y python=${PYTHON_VERSION}",
             "/opt/conda/bin/conda install -y",
         )
-    if "/opt/conda/bin/conda install -y" in line:
-        line = line.replace(
-            "/opt/conda/bin/conda install -y",
-            "/opt/conda/bin/conda tos accept --override-channels --channel "
-            "https://repo.anaconda.com/pkgs/main && "
-            "/opt/conda/bin/conda tos accept --override-channels --channel "
-            "https://repo.anaconda.com/pkgs/r && "
-            "/opt/conda/bin/conda install -y",
-        )
     patched.append(line)
 target.write_text("\n".join(patched) + "\n", encoding="utf-8")
 PY
